aae1cc85737b9946c8005f4d7d5b5daea6a96675
Abdullah BIYIK <abdullahb5355@gmail.com>
2022-02-22 00:54:37 +0300

    ulkeyegore-api-div-v-for

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<restApiComp/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import restApiComp from './components/rest-api-comp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    restApiComp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/rest-api-comp.vue
@@ -0,0 +1,84 @@
+<template>
+
+<div v-for="index in news.length" :key="index" class="kart" v-bind:id="index"  >
+    <div class="kart_photo"><img class="img-fluid" v-bind:src= "news[index-1].image"></div>
+    <div class="kart_baslik"><h1> {{news[index-1].name }} </h1></div>
+    <div class="kart_aciklama"><h2>{{ news[index-1].description }}</h2></div>
+    <div class="kart_zaman"><h3>{{ news[index-1].date }} &nbsp;&nbsp;&nbsp;&nbsp; {{ news[index-1].source }} | 4min read</h3>
+    <hr><div class="kart_like"><i class="fa-solid fa-heart"></i> &nbsp;&nbsp;&nbsp;&nbsp; <i class="fa-solid fa-arrow-up-from-bracket"></i>&nbsp;&nbsp;&nbsp;&nbsp;  <i class="fa-solid fa-bookmark"></i></div>
+    </div>        
+</div>
+
+</template>
+
+<script>
+
+import axios from 'axios'
+
+
+export default {
+  data(){
+    return {
+      news: null,
+    }
+  },
+  //async created() ve await axios get(dinamikIpString) 
+  async created(){
+  
+    var ulkeSonKullanici = "188.210.13.246";
+    // window.location.host
+
+    var dynamicIpString = "http://api.ipapi.com/"+ulkeSonKullanici+"?access_key=b8130a15504aba8cee89efafaa11061f"  
+        var ulkeKodu = await axios.get(dynamicIpString)
+        .then(function(response){
+          return response.data.country_code.toLowerCase().trim()
+        })
+        .catch(function(error){
+          console.log("errorvar"+error)
+        })
+      console.log(ulkeKodu)
+
+      var optionAxios = {
+          headers: {
+            "content-type":"application/json",
+            "authorization":"apikey 0v7dhJ1WwYlaR90Y6ErZdD:3T14eFz8OdWeS9N2z8SIV9"
+          }
+      }
+
+      this.news = await axios.get('https://api.collectapi.com/news/getNews?country='+ulkeKodu+'&tag=general',optionAxios)
+      .then( function(response)  {
+      console.log(response.data.result[0].name);
+      console.log(response.data.result[0].description);
+      console.log(response.data.result[0].date);
+      //lambda fonksiyon olunca name kısmı gelmiyor
+      return response.data.result
+    
+       })
+       .catch( function(error) {
+           console.log("errorvar"+error);
+           
+        });
+
+        console.log(this.news.length)
+    
+  //"CORS" kapatmak için...
+  /*
+  module.exports = {
+    devServer: {
+      proxy: 'http://localhost:8080'  --> 'api sunan sitenin ismini proxy yapilir' -> root/[vue.config.js]
+    }
+  }
+  */
+  //apikey 0v7dhJ1WwYlaR90Y6ErZdD:3T14eFz8OdWeS9N2z8SIV9
+  }
+    
+  
+
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
