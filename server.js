fea5abbad6f8f2248cd5493c4c724d6343aec10a
Abdullah BIYIK <abdullahb5355@gmail.com>
2022-02-22 23:22:04 +0300

    server.jsdeneme

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 
 
-app.use('/', express.static(path.join(__dirname, 'public')))
+app.use('/', express.static(path.join(__dirname, '/src/public')))
 
 
 const port = process.env.PORT || 8080
