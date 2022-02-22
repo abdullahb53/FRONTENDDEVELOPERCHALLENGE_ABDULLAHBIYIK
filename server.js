c3ad099163a2a73f5d01fb6bf1316bf89fac68fd
Abdullah BIYIK <abdullahb5355@gmail.com>
2022-02-22 23:48:50 +0300

    router??

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
 
 const express = require('express')
+const port = process.env.PORT || 8080;
+const app = express()
+
+app.use(express.static(__dirname+ "/dist/"));
+app.get(/.*/, function(req,res){
+    res.sendFile(__dirname+"/dist/index.html");
+});
+app.listen(port);
+
+console.log("Sunucu basladi..")
+
+
+
+/*
 const serveStatic = require('serve-static')
 const path = require('path')
 
@@ -16,3 +30,5 @@ const port = process.env.PORT || 8080
 
 app.listen(port)
 
+
+*/
